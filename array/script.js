6eb59bb1c45f33661bd8376ecb70fc18e4d3ca68
NamrataPawar25 <namratapawar9793@gmail.com>
2025-06-23 22:13:38 +0530

    object.js file added

--- array/script.js
+++ array/script.js
@@ -1,17 +1,26 @@
-let task= document.querySelector("#task")
+let task = document.querySelector("#task")
 let result1 = document.querySelector("#showResult1")
 let smark = document.querySelector("#marks");
 let result2 = document.querySelector("#showResult2");
+let text = document.querySelector("#arrayText")
+let result3 = document.querySelector("#showResult3")
+let number = document.querySelector("#number")
+let result4 = document.querySelector("#showResult4")
+let name1 = document.querySelector("#name1")
+let result5 = document.querySelector("#showResult5")
+let CreateRowField = document.querySelector("#CreateRow")
+
+
 
 let taskArray = [];
-function deleteTask(i){
+function deleteTask(i) {
     console.log(i)
     taskArray.splice(i, 1)
     displayTasks();
 }
-function displayTasks(){
-    
-    result1.innerHTML = taskArray.map((t, index)=>`
+function displayTasks() {
+
+    result1.innerHTML = taskArray.map((t, index) => `
                                     <li class="liClass">
                                     <span>${t}</span>
                                     <button class='btn btn-danger' onclick='deleteTask(${index})'>Delete</button>
@@ -23,19 +32,19 @@ function addTask() {
     let newTask = task.value.trim();
 
     if (newTask === "") {
-        result1.textContent= "Empty task not added"
+        result1.textContent = "Empty task not added"
         return;
     }
 
     taskArray.push(newTask);
-    task.value = ""; 
+    task.value = "";
     console.log("Task list:", taskArray);
     displayTasks()
 }
 
-function clearAll(){
-    taskArray=[]
-    task.value=""
+function clearAll() {
+    taskArray = []
+    task.value = ""
     displayTasks()
 }
 
@@ -44,7 +53,10 @@ let markarray = [];
 
 function addMark() {
     let newmark = smark.value.trim();
-    if (newmark === "") return;
+    if (newmark === "") {
+        result2.textContent = "Enter Mark"
+        return;
+    };
 
     markarray.push(newmark);
     smark.value = "";
@@ -88,3 +100,111 @@ function reset() {
     console.log("Marks reset");
 }
 
+let array = ["Audi", "Tata", "Fortuner", "Thar", "Scorpio"]
+function search() {
+    let newText = text.value.trim();
+    let match = array.find(item => item.toLowerCase() === newText.toLowerCase());
+
+    if (newText === "") {
+        result3.textContent = "Empty Array not found"
+        return;
+    }
+
+    if (match) {
+        result3.textContent = match;
+    } else {
+        result3.textContent = "No matches Found";
+    }
+}
+
+numberArray = []
+function addNumber() {
+    let newnumber = number.value.trim()
+    if (newnumber === "") {
+        result4.textContent = "Empty Number not added"
+        return;
+    }
+
+    numberArray.push(newnumber);
+    number.value = "";
+    console.log(numberArray);
+    result4.textContent = numberArray.join(", ");
+}
+function showEven() {
+    if (numberArray.length === 0) {
+        result4.innerText = "No number entered.";
+        return;
+    }
+    let evenNumbers = numberArray.filter(num => num % 2 === 0);
+
+    if (evenNumbers.length > 0) {
+        result4.innerText = "Even numbers: " + evenNumbers.join(", ");
+    } else {
+        result4.innerText = "No even numbers found.";
+    }
+}
+
+function showOdd() {
+    if (numberArray.length === 0) {
+        result4.innerText = "No number entered.";
+        return;
+    }
+
+
+    let oddNumbers = numberArray.filter(num => num % 2 !== 0);
+
+    if (oddNumbers.length > 0) {
+        result4.innerText = "Odd numbers: " + oddNumbers.join(", ");
+    } else {
+        result4.innerText = "No odd numbers found.";
+    }
+}
+
+function EVclear() {
+    numberArray = [];
+    number.value = "";
+    result4.textContent = ""
+}
+
+nameArray = []
+function addName() {
+    newName = name1.value
+    if (newName === "") {
+        result5.textContent = "Empty Names not added"
+        return;
+    }
+
+
+    nameArray.push(newName);
+    name1.value = "";
+    console.log(nameArray);
+    result5.textContent = nameArray.join(", ");
+}
+function sort() {
+    nameArray.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+    result5.textContent = nameArray.join(", ");
+}
+
+function NameTaskreset() {
+    nameArray = []
+    name1.value = ""
+    result5.textContent = ""
+}
+numArray=[2,3,4,5,6,7,8,9,10]
+function createTables() {
+    CreateRowField.innerHTML = numArray.map((x, i) => `
+                                <tr>
+                                <td>${x * 1}</td>
+                                <td>${x * 2}</td>
+                                <td>${x * 3}</td>
+                                <td>${x * 4}</td>
+                                <td>${x * 5}</td>
+                                <td>${x * 6}</td>
+                                <td>${x * 7}</td>
+                                <td>${x * 8}</td>
+                                <td>${x * 9}</td>
+                                <td>${x * 10}</td>
+                                </tr>
+    `).join("")
+}
+createTables()
---
+++ object.js
@@ -0,0 +1,65 @@
+student = {
+    name: "Namrata",
+    age: 20,
+    grade: 9.0,
+    subject: "Math",
+    grade: 9.5
+};
+console.log(student);
+delete student.age
+console.log(student.name);
+console.log(student.age);
+console.log(student.grade)
+console.log(student);
+
+book = {
+    title: "Programming in C",
+    author: "Dennis richhie",
+    details: {
+        pages: 100,
+        genre: "2nd"
+    }
+}
+console.log(book.details.pages);
+console.log(book.details.genre);
+
+product = {
+    Product_name: "parle",
+    price: 50,
+    stock: "True"
+}
+for (let key in product) {
+    if (key === "Product_name" && product[key] === "parle") {
+        console.log(product.Product_name, ":",product.stock);
+        break;
+    }
+}
+cal={
+    addition_function(a,b){
+        return a+b;
+    },
+    substraction_function(a,b){
+        return a-b;
+    },
+    multiplication_function(a,b){
+        return a*b
+    },
+    division_function(a,b){
+        return a/b
+    }
+}
+console.log("Addition:" ,cal.addition_function(2,3));
+console.log("Substraction:",cal.substraction_function(10,5));
+console.log("Multiplication:",cal.multiplication_function(10,2));
+console.log("Division", cal.division_function(25,5));
+
+calculator = {
+    num1: 20,
+    num2: 60
+}
+console.log("Addition: ", calculator.num1 + calculator.num2);
+console.log(`Addition: ${calculator.num1} + ${calculator.num2} = ${calculator.num1 + calculator.num2}`);
+console.log(`Substraction: ${calculator.num1} - ${calculator.num2} = ${calculator.num1 - calculator.num2}`);
+console.log(`Multiplication: ${calculator.num1} * ${calculator.num2} = ${calculator.num1 * calculator.num2}`);
+console.log(`Division: ${calculator.num1} / ${calculator.num2} = ${calculator.num1 / calculator.num2}`);
+
